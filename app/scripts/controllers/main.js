dd2ed22942ba4378517d2a25d002720297708d05
ihiram <joe@hiramsoftware.com>
2014-05-17 15:37:52 -0700

    Added DataController with instagram event

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,40 @@
 
 angular.module('socialwallApp')
   .controller('MainCtrl', function ($scope) {
+
+  	$scope.pictures = [];
+  	$scope.$on('instagram', function(e, posts){
+  		// Handle the new instragram posts
+  		// something like...
+
+  		posts.forEach(function(arr, index, elem){ // don't remember my js forEach...
+  			$scope.pictures.push(elem);
+  			console.log("Added picture");
+  			console.log(elem);
+  		})
+  	})
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+  });
+
+  angular.module('socialwallApp')
+  .controller('DataController', function ($scope, $log, $rootScope) {
+
+  	$scope.debugMessages = [];
+  	var socket = io.connect('http://socialwall-api.funnl.co');
+	//var socket = io.connect('http://localhost');
+	socket.on('instagram', function(jsonString){
+		$log.info("Got instagram update from api server");
+		$log.info(jsonString);
+		var jsonData = JSON.parse(jsonString);
+		$log.info(jsonData);
+		$scope.debugMessages.push(jsonData.data);
+
+		// The Instagram data is wrapped for pagination, which we aren't using
+		// so, we are just going to use the raw data
+		$rootScope.$broadcast('instagram', jsonData.data);
+	})
   });
