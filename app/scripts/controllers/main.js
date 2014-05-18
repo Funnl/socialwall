f392c38d244e2bfcfcca39fad112367a69aba026
Kevin Chau <kevin@kevinchau.org>
2014-05-17 17:09:49 -0700

    twitter now works

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,7 @@ angular.module('socialwallApp')
   .controller('MainCtrl', function ($scope) {
 
   	$scope.photos = [];
+    $scope.tweets = [];
   	$scope.$on('instagram', function(e, posts){
   		// Handle the new instragram posts
   		// something like...
@@ -13,131 +14,7 @@ angular.module('socialwallApp')
   			$scope.photos.push(photo);
   			console.log("Added photo");
   			console.log(JSON.stringify(photo));
-  			/*
-
-
-{
-
-   "attribution": null,
-
-   "tags": [
-
-      "angelhacktest"
-
-   ],
-
-   "location": {
-
-      "latitude": 47.62314,
-
-      "longitude": -122.330025
-
-   },
-
-   "comments": {
-
-      "count": 0,
-
-      "data": []
-
-   },
-
-   "filter": "Normal",
-
-   "created_time": "1400362896",
-
-   "link": "http://instagram.com/p/oHQfsKEz9x/",
-
-   "likes": {
-
-      "count": 0,
-
-      "data": []
-
-   },
-
-   "images": {
-
-      "low_resolution": {
-
-         "url": "http://origincache-ash.fbcdn.net/10387770_1511899072362967_848554503_a.jpg",
-
-         "width": 306,
-
-         "height": 306
-
-      },
-
-      "thumbnail": {
-
-         "url": "http://origincache-ash.fbcdn.net/10387770_1511899072362967_848554503_s.jpg",
-
-         "width": 150,
-
-         "height": 150
-
-      },
-
-      "standard_resolution": {
-
-         "url": "http://origincache-ash.fbcdn.net/10387770_1511899072362967_848554503_n.jpg",
-
-         "width": 640,
-
-         "height": 640
-
-      }
-
-   },
-
-   "users_in_photo": [],
-
-   "caption": {
-
-      "created_time": "1400362896",
-
-      "text": "#angelhacktest",
-
-      "from": {
-
-         "username": "thekchau",
-
-         "profile_picture": "http://images.ak.instagram.com/profiles/profile_271576230_75sq_1375628853.jpg",
-
-         "id": "271576230",
-
-         "full_name": "Kevin Chau"
-
-      },
-
-      "id": "722618811937931022"
-
-   },
-
-   "type": "image",
-
-   "id": "722618811677884273_271576230",
-
-   "user": {
-
-      "username": "thekchau",
-
-      "website": "",
-
-      "profile_picture": "http://images.ak.instagram.com/profiles/profile_271576230_75sq_1375628853.jpg",
-
-      "full_name": "Kevin Chau",
-
-      "bio": "",
-
-      "id": "271576230"
-
-   }
-
-}
-
-
-  			*/
+  			
   			var dt = moment(photo.caption.created_time);
   			photo.caption.friendly_time = dt.fromNow().toString();
   			console.log(photo.images.thumbnail.url);
@@ -145,6 +22,18 @@ angular.module('socialwallApp')
         $scope.$digest();
   		})
   	})
+    $scope.$on('twitter', function(e, tweet){
+      // Handle the new instragram posts
+      // something like...
+
+        $scope.tweets.unshift(tweet);
+        console.log("Added tweet");
+        console.log(JSON.stringify(tweet));
+        
+        var dt = moment(tweet.created_at);
+        tweet.friendly_time = dt.fromNow().toString();
+        $scope.$digest();
+      })
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -168,5 +57,11 @@ angular.module('socialwallApp')
 		// The Instagram data is wrapped for pagination, which we aren't using
 		// so, we are just going to use the raw data
 		$rootScope.$broadcast('instagram', jsonData.data);
-	})
+	});
+
+  socket.on('twitter', function(jsonString){
+    $log.info("Got twitter update from api server");
+    $log.info(jsonString);
+    $rootScope.$broadcast('twitter', jsonString);
+  });
   });
